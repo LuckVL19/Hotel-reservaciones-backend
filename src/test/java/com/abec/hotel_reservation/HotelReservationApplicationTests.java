f84f2b2efaa5cfd3019e092753519363783304a7
Alben Bustamante Barrios <albenbustamante@gmail.com>
2024-06-30 16:38:41 -0500

    initial commit

---
+++ src/test/java/com/abec/hotel_reservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abec.hotel_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
